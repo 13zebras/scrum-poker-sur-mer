b544aa231bfa0b296f4239b20a7cb1638dfe2995
Tom Stine <git@13z.one>
2024-08-11 01:28:42 -0500

    setup and architecture of app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	darkMode: ['class'],
+	content: [
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+		'./node_modules/daisyui/dist/**/*.js',
+		'./node_modules/react-daisyui/dist/**/*.js',
+	],
+	theme: {
+		extend: {
+			keyframes: {},
+			animation: {},
+		},
+	},
+	plugins: [daisyui],
+	// daisyUI config (these are the default values)
+	daisyui: {
+		themes: [
+			'night',
+			{
+				dark: {
+					...require('daisyui/src/theming/themes').dark,
+					'base-100': '#00001b',
+				},
+			},
+			'halloween',
+		],
+		darkTheme: 'dark', // name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		themeRoot: ':root', // The element that receives theme color CSS variables
+	},
+}
+
+export default config
