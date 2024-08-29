0ac805c64398913b280e816d3d8ff09eac6eef77
Tom Stine <13zebras@users.noreply.github.com>
2024-08-29 02:19:22 -0500

    Options object pattern on server and socket.tsx updated socketEmitter to work with options. Only emitter needed after some refactoring.

--- src/services/socketOn.js
+++ src/services/socketOn.js
@@ -1,25 +1,19 @@
 // This file is used to handle the socket.on event
 
 module.exports = (io, socket) => {
-	socket.onAny((eventName, roomId, ...args) => {
+	socket.onAny((eventName, options) => {
+		const { roomId, message, userName, timeStamp, ...otherOptions } = options
 		console.log('>> eventName:', eventName)
-		console.log('>> roomId:', roomId)
-		console.log('>> args:', args)
-		const [message, userName, timeStamp] = args
-		// console.log('>> message, userName:', message, userName)
+		console.log('>> options:', options)
 		console.log('**************************************************')
 		if (eventName === 'join-room') {
 			socket.join(roomId)
 		}
-		io.to(roomId).emit(eventName, message, userName, timeStamp, ...args)
+		io.to(roomId).emit(eventName, {
+			message: message,
+			userName: userName,
+			timeStamp: timeStamp,
+			...otherOptions,
+		})
 	})
 }
-
-// *** saving below for now, will delete later ***
-
-// socket.on('join-room', (message, roomId, userName) => {
-// 	console.log('>> join-room:', message, roomId, userName)
-// 	console.log('**************************************************')
-// 	socket.join(roomId)
-// 	io.to(roomId).emit('join-room', message, roomId, userName)
-// })
